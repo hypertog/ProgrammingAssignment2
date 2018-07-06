db1a873a78373d7c1901f7e9352262b5c035fe7a
hypertog <qdmabasa@gmail.com>
2018-07-06 17:55:14 -0400

    Yes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this captures the make cache matrix, which mirrors the cache mean with inverse operations in place of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i = NULL
+  set = function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get = function() x
+  setinverse = function(inverse) i <<- inverse 
+  getinverse = function() i
+  list(set=set, get=get, setinv=setinverse, getinv=getinverse)
 }
 
 
-## Write a short comment describing this function
+## this gets the cache values if there is a matrix already in the environment
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## @x: output of makeCacheMatrix()
+  ## return: inverse of the original matrix input to makeCacheMatrix()
+  
+  i = x$getinverse()
+  
+  # if the inverse has already been calculated
+  if (!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data = x$get()
+  i = solve(data, ...)
+  x$setinverse(i)
+  i
 }
